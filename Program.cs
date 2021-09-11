e040fe6d752c9d1453d5948a97effc379e3f56ec
kurban1998 <90529029+kurban1998@users.noreply.github.com>
2021-09-11 23:19:57 +0300

    S kruga

--- Program.cs
+++ Program.cs
@@ -5,14 +5,38 @@ namespace Файлы
 {
     class Program
     {
+        public static double S { get; set; }
+
         static void Main(string[] args)
         {
-           
-            using (var sw = new StreamWriter(@"C:\Users\SONY\Desktop\1.txt",true))
+            while (true)
             {
-                sw.Write("Hello");  
+                Console.Write("Введите радиус окружности: ");
+               
+                try
+                {
+                    double rad = Convert.ToDouble(Console.ReadLine());
+                    if (rad > 0)
+                    {
+                        S = Math.PI * Math.Pow(rad, 2); //Площадь круга
+                        Console.WriteLine($"Площадь круга равна {S} м. кв.");
+                    // записать в файл
+                        using (var sw = new StreamWriter(@"C:\Users\SONY\Desktop\1.txt", true))
+                        {
+                            sw.WriteLine($"Радиус окружности: {rad}, Площадь круга: {S} М. Кв. ");
+                            sw.Close();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число не может быть отрицательным");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверно введены данные");
+                }
             }
-            
         }
     }
 }
