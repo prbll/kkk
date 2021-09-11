8376523e17a677b5caa29a01a9a4a8261638a5c1
kurban1998 <90529029+kurban1998@users.noreply.github.com>
2021-09-11 22:54:46 +0300

    Files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Файлы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            using (var sw = new StreamWriter(@"C:\Users\SONY\Desktop\1.txt",true))
+            {
+                sw.Write("Hello");  
+            }
+            
+        }
+    }
+}
