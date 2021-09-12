78dbe734de0f8c88a8dd797aa53a7e20a15199a8
kurban1998 <90529029+kurban1998@users.noreply.github.com>
2021-09-12 15:51:47 +0300

    S kruga

--- Program.cs
+++ Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.IO;
 
-namespace BANKI_DZ
+namespace Файлы
 {
     class Program
     {
+        public static double S { get; set; }
+
         static void Main(string[] args)
         {
-            Class1 bank = new Class1();
-            bank.DataAboutPerson();
+            while (true)
+            {
+                Console.Write("Введите радиус окружности: ");
+               
+                try
+                {
+                    double rad = Convert.ToDouble(Console.ReadLine());
+                    if (rad > 0)
+                    {
+                        S = Math.PI * Math.Pow(rad, 2); //Площадь круга
+                        Console.WriteLine($"Площадь круга равна {S} м. кв.");
+                    // записать в файл
+                        using (var sw = new StreamWriter(@"C:\Users\SONY\Desktop\1.txt", true))
+                        {
+                            sw.WriteLine($"Радиус окружности: {rad}, Площадь круга: {S} М. Кв. ");
+                            sw.Close();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число не может быть отрицательным");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверно введены данные");
+                }
+            }
         }
     }
 }
